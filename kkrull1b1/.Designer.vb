92a303d6910488df9fc153197a8e67c0a5859bd7
graduate <graduate@STUDENTCOMPUTER>
2016-09-07 08:45:36 -0500

    Add project files.

---
+++ kkrull1b1/.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSplash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.picStorks = New System.Windows.Forms.PictureBox()
+        Me.PicIguanas = New System.Windows.Forms.PictureBox()
+        Me.tmrExit = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picStorks, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicIguanas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(180, 70)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(111, 21)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Crighton Zoo"
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblMsg.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.ForeColor = System.Drawing.Color.Red
+        Me.lblMsg.Location = New System.Drawing.Point(180, 115)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(191, 21)
+        Me.lblMsg.TabIndex = 1
+        Me.lblMsg.Text = "Come visit our residents!"
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(367, 283)
+        Me.Label1.TabIndex = 2
+        '
+        'picStorks
+        '
+        Me.picStorks.Image = Global.kkrull1b1.My.Resources.Resources.Storks
+        Me.picStorks.Location = New System.Drawing.Point(212, 139)
+        Me.picStorks.Name = "picStorks"
+        Me.picStorks.Size = New System.Drawing.Size(159, 150)
+        Me.picStorks.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picStorks.TabIndex = 4
+        Me.picStorks.TabStop = False
+        '
+        'PicIguanas
+        '
+        Me.PicIguanas.Image = Global.kkrull1b1.My.Resources.Resources.Iguanas
+        Me.PicIguanas.Location = New System.Drawing.Point(15, 12)
+        Me.PicIguanas.Name = "PicIguanas"
+        Me.PicIguanas.Size = New System.Drawing.Size(159, 150)
+        Me.PicIguanas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicIguanas.TabIndex = 3
+        Me.PicIguanas.TabStop = False
+        '
+        'tmrExit
+        '
+        Me.tmrExit.Enabled = True
+        Me.tmrExit.Interval = 4000
+        '
+        'frmSplash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(389, 302)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.picStorks)
+        Me.Controls.Add(Me.PicIguanas)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmSplash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.picStorks, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicIguanas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PicIguanas As PictureBox
+    Friend WithEvents picStorks As PictureBox
+    Friend WithEvents tmrExit As Timer
+End Class
