43ab434ab735c030bb628ed1768b0810e8ee3d3c
graduate <graduate@STUDENTCOMPUTER>
2016-09-11 11:16:33 -0500

    Added Ex1d: Bakery

---
+++ kkrull1d1/Main Form.vb
@@ -0,0 +1,32 @@
+﻿' Nme:          Bakery Project
+'Purpose:       Calucates the total number of items sold and total sales
+'Programmer:    Kyle Krull on 8/30/2017
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' prepare screen for the next sale
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        txtDonuts.Focus()
+
+        ' send the focus to the doughnuts box
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculate number of itmes sold and total sales
+
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(txtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.5
+        lblTotalSales.Text = Format(lblTotalSales.Text, "currency")
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
